5ef2fdf4f39cc710c23442849896cab24f037dd7
suyeong2785 <qkdtpwls_@naver.com>
2024-01-02 18:27:28 +0900

    서비스를 포그라운드 서비스로 실행시켜서 앱을 나가도 노티피케이션을 통해서 음악을 중지,재시작,종료하도록 기능 구현

--- app/src/main/java/com/example/chapter9/MainActivity.kt
+++ app/src/main/java/com/example/chapter9/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.chapter9
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.media.MediaPlayer
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.chapter9.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private var mediaPlayer : MediaPlayer? = null
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,35 +29,49 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun mediaPlayerPlay() {
+        checkPermission()
         val intent = Intent(this, MediaPlayerService::class.java)
             .apply { action = MEDIA_PLAYER_PLAY }
         startService(intent)
-
-        /*        if (mediaPlayer == null) {
-            mediaPlayer = MediaPlayer.create(this,R.raw.music).apply {
-                isLooping = true
-            }
-        }
-        mediaPlayer?.start()*/
     }
     private fun mediaPlayerPause() {
         val intent = Intent(this, MediaPlayerService::class.java)
             .apply { action = MEDIA_PLAYER_PAUSE }
         startService(intent)
-
-        mediaPlayer?.pause()
     }
     private fun mediaPlayerStop() {
         val intent = Intent(this, MediaPlayerService::class.java)
             .apply { action = MEDIA_PLAYER_STOP }
         startService(intent)
-
-        mediaPlayer?.stop()
-        mediaPlayer?.release()
-        mediaPlayer = null
     }
 
+    private fun checkPermission() {
+        when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) -> {
+                Log.d("checkPermission", "permission denied")}
+            else -> {
+                requestNotification()
+            }
+        }
+    }
 
+    private fun requestNotification() {
+        Log.d("checkPermission", "requestNotification execute!!")
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.POST_NOTIFICATIONS),
+            POST_NOTIFICATIONS
+        )
+    }
 
-
+    override fun onDestroy() {
+        stopService(Intent(this,MediaPlayerService::class.java))
+        super.onDestroy()
+    }
+    companion object {
+        const val POST_NOTIFICATIONS = 100
+    }
 }
