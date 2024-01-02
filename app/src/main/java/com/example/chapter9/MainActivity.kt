7125cc77912bd722f35cf14b0b748ca4f2174f8c
suyeong2785 <qkdtpwls_@naver.com>
2024-01-02 13:00:32 +0900

    23/12/31 방수영 1.음악 재생,일시정지,종료 기능 구현

--- app/src/main/java/com/example/chapter9/MainActivity.kt
+++ app/src/main/java/com/example/chapter9/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.chapter9
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chapter9.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var mediaPlayer : MediaPlayer? = null
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.playButton.setOnClickListener { mediaPlayerPlay() }
+        binding.pauseButton.setOnClickListener { mediaPlayerPause() }
+        binding.stopButton.setOnClickListener { mediaPlayerStop() }
     }
+
+    private fun mediaPlayerPlay() {
+        if (mediaPlayer == null) {
+            mediaPlayer = MediaPlayer.create(this,R.raw.music).apply {
+                isLooping = true
+            }
+        }
+        mediaPlayer?.start()
+    }
+    private fun mediaPlayerPause() {
+        mediaPlayer?.pause()
+    }
+    private fun mediaPlayerStop() {
+        mediaPlayer?.stop()
+        mediaPlayer?.release()
+        mediaPlayer = null
+    }
+
+
+
+
 }
